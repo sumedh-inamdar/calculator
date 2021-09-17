86edbb68a168724d74824f58822a37cb14295700
Sumedh Inamdar <sinamdar@gmail.com>
2021-09-16 18:38:13 -0700

    Equal handler + operationHandler for nominal use cases completed

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ To do list:
 JS
 - [x] Create functions for math operators
 - [x] Create function "operate" that takes 2 numbers and operator and calls operator above
-- [ ] Create function to populate display when number button is clicked and update display variable 
+- [x] Create function to populate display when number button is clicked and update display variable 
 - [ ] Store display number and operation once operator is triggered
 - [ ] Call operate once second number 
 - [ ] Clear button: 1st click for clearing mainDisp, 2nd click for clearing LHS_Disp
--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+// Global variables
+let calc = {
+    operand1: null,
+    operand2: null,
+    operatorTarget: null,
+    total: null
+}
+
 // Button Nodes
 const clear = document.querySelector('#clear');
 const plusMinus = document.querySelector('#plusMinus');
@@ -6,54 +14,101 @@ const backspace = document.querySelector('#backspace');
 const nums = document.querySelectorAll('[name="number"]');
 const decimal = document.querySelector('[value="."]');
 const equals = document.querySelector('#equals');
-const plusNode = document.querySelector('#plus');
-const minusNode = document.querySelector('#minus');
-const multiplyNode = document.querySelector('#multiply');
-const divideNode = document.querySelector('#divide');
-const powerNode = document.querySelector('#power');
-const factorialNode = document.querySelector('#factorial');
+const operators = document.querySelectorAll('.basicOper');
+
+// const powerNode = document.querySelector('#power');
+// const factorialNode = document.querySelector('#factorial');
 
 // Display Nodes
 const mainDisp = document.querySelector('.mainDisp p');
+const miniDisp = document.querySelector('.LHS_Disp');
 
 // Add event listeners for buttons
-nums.forEach(num => {
-    num.addEventListener('click', dispHandler);
+plusMinus.addEventListener('click', () => {
+    mainDisp.textContent *= -1;
 });
-
+nums.forEach(num => {
+    num.addEventListener('click', numberHandler);
+});
 decimal.addEventListener('click', (e) => {
     if (!mainDisp.textContent.includes('.')) {
-        dispHandler(e);
+        if (mainDisp.textContent === '') {
+            mainDisp.textContent += '0';
+        }
+        numberHandler(e);
     }
-})
+});
+equals.addEventListener('click', equalHandler);
+operators.forEach(oper => {
+    oper.addEventListener('click', operationHandler)
+});
+
 
 // Event handler functions
-function dispHandler(event) {
-    if (mainDisp.textContent == '0' && event.target.value !== '.') {
+function numberHandler(event) {
+    if (mainDisp.textContent == '') {
         mainDisp.textContent = event.target.value;
     } else {
         mainDisp.textContent += event.target.value;
     }
-    
+}
+function equalHandler(event) {
+    if(calc.operand1 && mainDisp.textContent && calc.operatorTarget) {
+        storeMainDispValIn('operand2');
+        calc.total = operate(window[calc.operatorTarget.value], calc.operand1, calc.operand2);
+        updateMiniDisp();
+        updateMainDisp(calc.total);
+    }
+}
+function operationHandler(event) {    
+    if (mainDisp.textContent || miniDisp.textContent) { // ignore click if no operand input yet
+        if (!calc.operand1) {
+            storeMainDispValIn('operand1');
+        } else if (!calc.operand2) {
+            storeMainDispValIn('operand2');
+            calc.total = operate(window[calc.operatorTarget.value], calc.operand1, calc.operand2);
+            calc.operand1 = calc.total;
+            calc.operand2 = null;
+        } else {
+            return;
+        }
+        calc.operatorTarget = event.target;
+        updateMiniDisp();
+        updateMainDisp(null);
+        
+    }
 }
 
+// helper functions
+function storeMainDispValIn(key) {
+    calc[key] = Number(mainDisp.textContent);
+}
+function updateMiniDisp() {
+    miniDisp.textContent = calc.operand1 + ' ' + calc.operatorTarget.textContent + ' '; 
+    miniDisp.textContent += calc.operand2 ? calc.operand2 + ' = ': '';
+}
+function updateMainDisp(str) {
+    mainDisp.textContent = str;
+}
+
+
 // Operator functions
-const add = function(a, b) {
-    return a + b;
+function add(a, b) {
+    return  a + b;
 };
-const subtract = function(a, b) {
+function subtract(a, b) {
     return a - b;
 };
-const multiply = function(a, b) {
+function multiply(a, b) {
     return a * b;
 }
-const divide = function(a, b) {
+function divide(a, b) {
     return a / b;
 }
-const power = function(a, b) {
+function power(a, b) {
 	return a ** b;
 };
-const factorial = function(num) {
+function factorial(num) {
 	let total = 1;
   while (num > 0) {
     total *= num;
@@ -62,9 +117,10 @@ const factorial = function(num) {
   return total;
 
 };
-const operate = function(operator, a, b) {
+function operate(operator, a, b) {
     return operator(a, b);
 }
 
 // Startup sequence
-mainDisp.textContent = '0';
+mainDisp.textContent = null;
+miniDisp.textContent = null;
